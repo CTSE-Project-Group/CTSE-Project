e4d1766de2eed7f11fae970efba133ee60bcce95
lkh-krishen <lkhkrishen@gmail.com>
2023-03-20 01:19:09 +0530

    Added initial files

---
+++ firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyATNVgZmprp-qQl9ZytOijfToFabZU51GE",
+    authDomain: "todoapp-ca71d.firebaseapp.com",
+    projectId: "todoapp-ca71d",
+    storageBucket: "todoapp-ca71d.appspot.com",
+    messagingSenderId: "93799141888",
+    appId: "1:93799141888:web:1df8321e0bd683acf8fee5",
+    measurementId: "G-PH6NJHQ2B0"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore();
+
+export {
+    auth,
+    db
+}
---
+++ screens/Login.js
@@ -0,0 +1,63 @@
+import { Text, View, ImageBackground, TextInput, Button, KeyboardAvoidingView, Platform } from 'react-native';
+import AppStyles from '../styles/AppStyles';
+import React from 'react';
+import InlineTextButton from '../components/inlineTextButton';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth, currentUser } from '../firebase';
+
+export default function Login({ navigation }) {
+    const background = require("../assets/background.jpg");
+
+    if (auth.currentUser) {
+        navigation.navigate("ToDo");
+    }
+
+    let [errorMessage, setErrorMessage] = React.useState("");
+    let [email, setEmail] = React.useState("");
+    let [password, setPassword] = React.useState("");
+
+    let login = () => {
+        if (email !== "" && password !== "") {
+            signInWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    navigation.navigate("ToDo", { user: userCredential })
+                })
+                .catch((error) => {
+                    setErrorMessage(error.message)
+                });
+        } else {
+            setErrorMessage("Please enter an email and password.");
+        }
+    }
+    return (
+        <ImageBackground style={AppStyles.imageContainer} source={background}>
+            <KeyboardAvoidingView style={AppStyles.backgroundCover} behavior={Platform.OS === "ios" ? "padding" : "null"}
+                keyboardVerticalOffset={60}>
+                <Text style={[AppStyles.lightText, AppStyles.header]}>Login</Text>
+                <Text style={AppStyles.errorText}>{errorMessage}</Text>
+                <TextInput
+                    style={[AppStyles.textInput, AppStyles.lightTextInput, AppStyles.lightText]}
+                    placeholder='Email'
+                    placeholderTextColor="#BEBEBE"
+                    value={email}
+                    onChangeText={setEmail} />
+                <TextInput
+                    style={[AppStyles.textInput, AppStyles.lightTextInput, AppStyles.lightText]}
+                    placeholder='Password'
+                    placeholderTextColor="#BEBEBE"
+                    secureTextEntry={true}
+                    value={password}
+                    onChangeText={setPassword} />
+                <View style={[AppStyles.rowContainer, AppStyles.topMargin]}>
+                    <Text style={AppStyles.lightText}>Don't have an account? </Text>
+                    <InlineTextButton text="Sign Up" onPress={() => navigation.navigate("SignUp")} />
+                </View>
+                <View style={[AppStyles.rowContainer, AppStyles.bottomMargin]}>
+                    <Text style={AppStyles.lightText}>Forgot your password? </Text>
+                    <InlineTextButton text="Reset" onPress={() => navigation.navigate("ResetPassword")} />
+                </View>
+                <Button title="Login" onPress={login} color="#f7b267" />
+            </KeyboardAvoidingView>
+        </ImageBackground>
+    );
+}
