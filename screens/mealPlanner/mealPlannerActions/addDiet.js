3a0848f4496ad09a672fa3fea1e1747eb3dbf602
Buddimal-Liyanapathirana <buddimalliyanapathirana@gmail.com>
2023-03-23 17:26:55 +0530

    deletd add diet new

--- screens/mealPlanner/mealPlannerActions/addDiet.js
+++ screens/mealPlanner/mealPlannerActions/addDiet.js
@@ -1,31 +1,107 @@
-import { Button } from "@rneui/base";
+import { Button as Btn } from "@rneui/base";
 import React, { useState } from "react";
-import { View, StyleSheet, Text, TextInput } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import { db } from "../../../firebase";
+import { Styles } from "../../../styles/CardStyles";
 import DefaultScreenStyles from "../../../styles/DefaultScreenStyles";
+import { StylesLocal } from "../../../styles/LocalStyles";
+import {
+  Avatar,
+  Button,
+  Card,
+  Title,
+  Paragraph,
+  TextInput,
+  Text,
+  List,
+} from "react-native-paper";
+import Icon from "react-native-vector-icons/Ionicons";
 
-const AddDiet = ({ navigation, props }) => {
+const AddDietNew = ({ navigation, props }) => {
+  const [userId, setUserID] = useState("");
   const [name, setName] = useState("");
+  const [desc, setDesc] = useState("");
+  const [instruct, setInstruct] = useState("");
+  const [morning, setBreakfast] = useState([]);
+  const [lunch, setLunch] = useState([]);
+  const [dinner, setDinner] = useState([]);
+  const [numberOfTextFields, setNumberOfTextFields] = useState(1);
+
+  const renderTextFields = () => {
+    const textFields = [];
+
+    for (let i = 0; i < numberOfTextFields; i++) {
+      textFields.push(
+        <TextInput key={i} placeholder={`Text field ${i + 1}`} />
+      );
+    }
+    return textFields;
+  };
+
+  const handleAddTextField = () => {
+    setNumberOfTextFields(numberOfTextFields + 1);
+  };
+
+  const insertLabel = (labelValue, style) => (
+    <Text style={style}>{labelValue}</Text>
+  );
 
   return (
-    <View style={DefaultScreenStyles.container}>
-      <Text>Add Diets</Text>
-      <TextInput
-        placeholder="Name"
-        placeholderTextColor="#BEBEBE"
-        value={name}
-        onChangeText={setName}
-      />
-    </View>
+    <Card style={Styles.cardContainer}>
+      {/* <Card.Title style={Styles.cardTitleStyle}>EEEE</Card.Title> */}
+      <Card.Content style={Styles.cardContent}>
+        <Text style={StylesLocal.cardTitle}>Add Pickup Details</Text>
+        <ScrollView style={Styles.scrollViewBasicStyle}>
+          <View>
+            <TextInput
+              activeUnderlineColor="#739713"
+              label={insertLabel("Diet name", StylesLocal.inputLabel)}
+              placeholder="insert diet name"
+              // onChangeText={(text) => setOrderName(text)}
+              style={StylesLocal.inputField}
+              maxLength={20}
+            />
+            <TextInput
+              activeUnderlineColor="#739713"
+              label={insertLabel("Description", StylesLocal.inputLabel)}
+              placeholder="insert description"
+              // onChangeText={(text) => setCategory(text)}
+              style={StylesLocal.inputField}
+              maxLength={15}
+            />
+
+            <TextInput
+              activeUnderlineColor="#739713"
+              label={insertLabel("Instructions", StylesLocal.inputLabel)}
+              placeholder="insert instructions"
+              // onChangeText={(text) => setCategory(text)}
+              style={StylesLocal.inputField}
+              maxLength={15}
+            />
+
+            <TextInput
+              activeUnderlineColor="#739713"
+              label={insertLabel("Category", StylesLocal.inputLabel)}
+              placeholder="insert order name"
+              // onChangeText={(text) => setCategory(text)}
+              style={StylesLocal.inputField}
+              maxLength={15}
+            />
+          </View>
+        </ScrollView>
+      </Card.Content>
+      <Card.Actions style={Styles.cardActionsStyle}>
+        <Button
+          uppercase={false}
+          // onPress={logger}
+          // onPress={navigator}
+          style={Styles.buttonProceed}
+        >
+          <Text style={Styles.text}> Proceed</Text>
+        </Button>
+      </Card.Actions>
+    </Card>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
-
-export default AddDiet;
+export default AddDietNew;
